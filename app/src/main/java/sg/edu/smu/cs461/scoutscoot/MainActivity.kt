01c845494e0aa7239a8b9dce093b94edac3a25fe
gjiayiing <jygoh.2019@sis.smu.edu.sg>
2023-02-27 10:36:47 +0800

    Project Setup

---
+++ app/src/main/java/sg/edu/smu/cs461/scoutscoot/MainActivity.kt
@@ -0,0 +1,11 @@
+package sg.edu.smu.cs461.scoutscoot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
