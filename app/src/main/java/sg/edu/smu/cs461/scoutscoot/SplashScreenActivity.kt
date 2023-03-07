a7b918184c6773f90c0da723a712a83c21cdcdbe
gjiayiing <jygoh.2019@sis.smu.edu.sg>
2023-03-08 02:58:21 +0800

    splash screen

---
+++ app/src/main/java/sg/edu/smu/cs461/scoutscoot/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package sg.edu.smu.cs461.scoutscoot
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val splashIcon = findViewById<ImageView>(R.id.splash)
+        splashIcon.alpha = 0f
+        splashIcon.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
