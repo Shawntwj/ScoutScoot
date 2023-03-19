25bc73368b795b963207e8d246a04c97a0d7745b
thetrisha <72932089+thetrisha@users.noreply.github.com>
2023-03-19 22:53:01 +0800

    retrieve profile and transaction history
    only skeleton code (havent test)

--- app/src/main/java/sg/edu/smu/cs461/scoutscoot/Profile.kt
+++ app/src/main/java/sg/edu/smu/cs461/scoutscoot/Profile.kt
@@ -3,11 +3,20 @@ package sg.edu.smu.cs461.scoutscoot
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import sg.edu.smu.cs461.scoutscoot.databinding.FragmentProfileBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -25,19 +34,77 @@ class Profile : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
     private lateinit var user: FirebaseAuth
+    private lateinit var database: DatabaseReference
+
+    // need to change this
+    val DATABASE_URL = ""
+
+    private lateinit var myAdapter:ArrayAdapter<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentProfileBinding.inflate(inflater, container, false )
+        return binding.root
+    }
 
+    override fun onViewCreated(
+        view: View, savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
         user = FirebaseAuth.getInstance()
 
         if(user.currentUser !=null){
             user.currentUser?.let {
                 binding.userName.text = it.email
             }
+            database = Firebase.database(DATABASE_URL).reference
+            database.keepSynced(true)
+
+            // Create the search query.
+            val query = database.child("rides").child(FirebaseAuth.getInstance().currentUser!!.uid)
+            query.get().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val snapshot = task.result
+                    Log.i("Profile", snapshot.toString())
+                    val name = snapshot.child("name").getValue(String::class.java)
+
+                    with(binding) {
+                        userName.setText(name)
+                    }
+                } else {
+                    Log.i("Profile", task.exception!!.message!!) //Don't ignore potential errors!
+                }
+            }
+
+            // Execute a query in the database to fetch appropriate data.
+            val options = FirebaseRecyclerOptions.Builder<Profile>()
+                .setQuery(query
+                ) { snapshot ->
+                    val id = snapshot.child("id").getValue(String::class.java)
+                    val status = snapshot.child("status").getValue(String::class.java)
+                    val startTime = snapshot.child("start_time").getValue(Long::class.java)
+                    val endTime = snapshot.child("end_time").getValue(Long::class.java)
+                    val rentalTime = snapshot.child("rental_time").getValue(Long::class.java)
+                    val initialLat = snapshot.child("initial_lat").getValue(Double::class.java)
+                    val initialLong = snapshot.child("initial_long").getValue(Double::class.java)
+                    val currentLat = snapshot.child("current_lat").getValue(Double::class.java)
+                    val currentLong = snapshot.child("current_long").getValue(Double::class.java)
+                    val price = snapshot.child("price").getValue(Double::class.java)
+                    val userId = snapshot.child("user_id").getValue(String::class.java)
+                    val scooterId = snapshot.child("scooter_id").getValue(Int::class.java)
+
+                    Profile()
+                }
+                .setLifecycleOwner(this)
+                .build()
+
+//            myAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options)
+//            val lv = findViewById<ListView>(R.id.transactionsLV)
+//            lv.adapter = myAdapter
+
+
         }
 
         binding.logout.setOnClickListener{
@@ -45,8 +112,9 @@ class Profile : Fragment() {
             startActivity(Intent(activity,LoginActivity::class.java))
             activity?.finish()
         }
-        return binding.root
     }
+
+
 
     companion object {
         /**
